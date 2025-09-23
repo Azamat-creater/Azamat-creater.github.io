37e9c644b1704d90bf216245fee9a0985498067f
Azamat-creater <azamat.aripdjanov@stu.cvccworks.edu>
2025-09-23 14:06:17 -0400

    Add files via upload

---
+++ projects/project02/project01-02.js
@@ -0,0 +1,21 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-2
+
+    Author: Azamat
+    Date: 9/23/2025
+
+    Filename: project01-02.js
+*/
+
+//define variables for service name and service speed
+		<script>
+			var service1Name = "Basic";
+			var service2NAme = "Express";
+			var service3Name = "Extreme";
+			var service4Name = "Ultimate";
+			var service1Speed = "0 Mbps";
+			var service2Speed = "100 Mbps";
+			var service3Speed = "500 Mbps";
+			var service4Speed = "1 Gig";
+		</script>
---
+++ projects/project04/project01-04_txt.js
@@ -0,0 +1,12 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-4
+
+    Author: Azamat
+    Date: 9/23/2025  
+
+    Filename: project01-04.js
+*/
+
+//define variables for home and work addresses
+
