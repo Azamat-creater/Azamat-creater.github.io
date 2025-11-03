2fd04fc5a67e8b9f599dcc3ee078a04bb9bebd5e
Azamat-creater <azamat.aripdjanov@stu.cvccworks.edu>
2025-11-03 13:51:07 -0500

    Add files via upload

---
+++ projects/chapter3/js03_txt.js
@@ -0,0 +1,12 @@
+/*    JavaScript 7th Edition
+     Chapter 3
+     Chapter case
+
+     Tipton Turbines
+     Program to display games results in a web table
+     Author: Azamat
+     Date: 11/3/2025  
+
+     Filename: js03.js
+ */
+
